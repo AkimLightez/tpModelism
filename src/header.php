5cec6753b669329a8477f4c4e967f2265ecc390d
AkimLightez <60650358+AkimLightez@users.noreply.github.com>
2020-06-12 09:39:13 +0200

    change

--- src/header.php
+++ src/header.php
@@ -2,22 +2,22 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Akim</title>
+    <title>TP modelisme</title>
 
 
 
 
 <link href="css/bootstrap.css" rel="stylesheet" >
-<link href="css/akim.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
 
   </head>
   <body class="text-center  d-flex flex-column">
 
   <header class="masthead mb-auto  ">
   <div class="inner">
-      <h3 class="masthead-brand">Akim</h3>
+      <h3 class="masthead-brand"><img src="images/ed.png" alt=""></h3>
       <nav class="nav nav-masthead justify-content-center">
-        <a class="nav-link active" href="index.php">Accueil</a>
+        <a class="nav-link" href="index.php">Accueil</a>
         <a class="nav-link" href="liste_commandes.php">List</a>
         <a class="nav-link" href="create_customer.php">Client</a>
         <a class="nav-link" href="createo.php">Commande</a>
